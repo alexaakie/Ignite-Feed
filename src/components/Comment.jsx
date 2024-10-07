d6d315d6ae3606b487f3e732d34cd4b7af324c27
Alexsandro Fischer <alex@i9ss.com.br>
2024-10-07 19:21:32 -0300

    css-module

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,18 +3,18 @@ import styles from './Comment.module.css'
 
 export function Comment() {
     return (
-        <div className={styles.coment}>
-        <img src="https://github.com/alexaakie.png"/>
+        <div className={styles.comment}>
+        <img src="https://github.com/alexaakie.png" alt=""/>
 
         <div className={styles.commentBox}>
         <div className={styles.commentContent}>
             <header>
                 <div className={styles.authorAndTime}>
                     <strong>Alex</strong>
-                    <time title='11 de maio às 08:13' datetime="2022-05-11 08:15:00"></time>
+                    <time title='11 de maio às 08:13' datetime="2022-05-11 08:15:00">Publicado há 1h</time>
                 </div>
                 <button Tittle='Trash'>
-                    <Trash size={20}/>
+                    <Trash size={24}/>
                 </button>
             </header>
             <p>
